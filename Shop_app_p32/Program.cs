faf81164ea95f4507d3090ac0cc194023e591564
Mario <mario@gmail.com>
2025-12-13 17:16:44 +0200

    add settings identity

--- Shop_app_p32/Program.cs
+++ Shop_app_p32/Program.cs
@@ -16,10 +16,22 @@ namespace Shop_app_p32
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => 
-            { 
-                
-            });
+            builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+            {
+                // Require email confirmation for login
+                options.SignIn.RequireConfirmedEmail = true;
+
+                // Set custom password requirements
+                options.Password.RequireDigit = false; // No digit required
+                options.Password.RequireNonAlphanumeric = false; // No special characters required
+                options.Password.RequiredLength = 4; // Minimum length of 4 characters
+                options.Password.RequireUppercase = false; // No uppercase letter required
+                options.Password.RequireLowercase = false; // No lowercase letter required
+                options.Password.RequiredUniqueChars = 0; // No unique characters required
+            })
+            .AddRoles<IdentityRole>() // Add role management support to Identity
+            .AddEntityFrameworkStores<UserContext>(); // Store Identity data in UserContext with Entity Framework
+
 
             builder.Services.AddControllersWithViews();
             var app = builder.Build();
