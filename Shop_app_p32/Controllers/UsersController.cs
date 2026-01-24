30ead4662d403570a5951c5af1017d31d3c0cbff
Mario <mario@gmail.com>
2026-01-24 17:41:25 +0200

    Add register and auth

---
+++ Shop_app_p32/Controllers/UsersController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Shop_app_p32.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private RoleManager<IdentityRole> _roleManager;
+        public UsersController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+        [HttpGet]
+        public IActionResult Auth()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Auth(string email, string password)
+        {
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            {
+                return BadRequest("Auth Email or password error ...");
+            }
+            var result = await _signInManager.PasswordSignInAsync(
+                    email,
+                    password,
+                    isPersistent: true,
+                    lockoutOnFailure: false
+                );
+            if(result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return BadRequest("Auth e-mail or password are error ...");
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(string email,  string password)
+        {
+            if(string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            {
+                return BadRequest("Register Email or password error ...");
+            }
+            var user = new IdentityUser
+            {
+                UserName = email,
+                Email = email,
+                EmailConfirmed = true
+            };
+            await _userManager.CreateAsync(user, password);
+            return Ok("User is regestered successfully ...");
+        }
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
