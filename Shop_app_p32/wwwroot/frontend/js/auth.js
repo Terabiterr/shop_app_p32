0487e9c442920f7209e3d940a7326fc94f72bd46
Mario <mario@gmail.com>
2026-04-18 17:37:00 +0300

    added forms to frontend

--- Shop_app_p32/wwwroot/frontend/js/auth.js
+++ Shop_app_p32/wwwroot/frontend/js/auth.js
@@ -1,5 +1,37 @@
 const url_server = `http://localhost:5080/api`
 
+document.addEventListener('DOMContentLoaded', function() {
+    const form_login = document.getElementById('form_login')
+    const form_register = document.getElementById('form_register')
+    form_login.style.display = 'none'
+    form_register.style.display = 'none'
+})
+
+function show_form_register() {
+    const form_login = document.getElementById('form_login')
+    const form_register = document.getElementById('form_register')
+    form_login.style.display = 'none'
+    form_register.style.display = 'block'
+}
+
+function show_form_login() {
+    const form_login = document.getElementById('form_login')
+    const form_register = document.getElementById('form_register')
+    form_login.style.display = 'block'
+    form_register.style.display = 'none'
+}
+
+function register() {
+    const username = document.getElementById('input_register_username_id').value
+    const email = document.getElementById('input_register_email_id').value
+    const password = document.getElementById('input_register_password_id').value
+    console.log(`Username: ${username}, E-mail: ${email}, Password: ${password}`)
+}
+
+function login() {
+
+}
+
 async function getToken(username, email, password) {
     const url_auth = `http://localhost:5080/api/apiusers/login`
     return await fetch(
